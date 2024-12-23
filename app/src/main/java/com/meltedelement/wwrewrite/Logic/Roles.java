f38fd54a5ec4743b26685ba1d638e77bf5a14279
meltedelement <meltedelement@gmail.com>
2024-12-23 15:54:40 +0000

    Filled role list

---
+++ app/src/main/java/com/meltedelement/wwrewrite/Logic/Roles.java
@@ -0,0 +1,52 @@
+package com.meltedelement.wwrewrite.Logic;
+
+public enum Roles {
+    //Werewolves
+    WEREWOLF,
+    CUBWOLF,
+    SORCERER,
+    MISTWOLF,
+    HEXWOLF,
+
+    //Villager/Atheist
+    VILLAGER,
+    ATHEIST,
+
+    //Town investigative
+    SEER,
+    AURASEER,
+    PI,
+    CLOCKMAKER,
+    GHOST,
+    TRACKER,
+    LOOKOUT,
+    EMPATH,
+
+    //Town protective
+    BODYGUARD,
+    WITCH,
+    ESCORT,
+    TRANSPORTER,
+
+    //Town killing
+    DEPUTY,
+    VIGILANTE,
+    VETERAN,
+    HUNTER,
+
+    //Town negative
+    CUPID,
+    LYCAN,
+    CURSED,
+
+    //Neutral Evil (Show up to seer/clock)
+    ARSONIST,
+    PESTILENCE,
+    SERIALKILLER,
+
+    //Neutral (Dont show up to seer/clock)
+    DOPPLEGANGER,
+    TANNER,
+    EXECUTIONER,
+    GRAVEDIGGER,
+}
